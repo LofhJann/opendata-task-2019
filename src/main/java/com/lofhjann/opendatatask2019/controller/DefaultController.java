c0b6c9d64533efb51487ad6991835cdb9b788522
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2019-03-31 16:58:16 +0300

    Prototype of History View

--- src/main/java/com/lofhjann/opendatatask2019/controller/DefaultController.java
+++ src/main/java/com/lofhjann/opendatatask2019/controller/DefaultController.java
@@ -1,6 +1,9 @@
 package com.lofhjann.opendatatask2019.controller;
 
+import com.lofhjann.opendatatask2019.service.SensorDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,17 +14,24 @@ import javax.transaction.Transactional;
 @Transactional
 public class DefaultController {
 
+    @Autowired
+    private SensorDataService sensorDataService;
+
     @GetMapping("*")
     @ResponseBody
     public String error() {
-
-
         return "404 - Page not found";
     }
 
     @GetMapping("/")
     public String home() {
-        return "/index.html";
+        return "index";
+    }
+
+    @GetMapping("/history")
+    public String history(Model model) {
+        model.addAttribute("sensordata", sensorDataService.getAll());
+        return "history";
     }
 }
 
