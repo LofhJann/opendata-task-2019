f758f15f06176b5d8e5b41eca6f7deb444547279
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2019-04-01 00:39:55 +0300

    More comprehensive Mock data for chart development

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -6,3 +6,66 @@ VALUES (2, '2019-03-31 13:00:00', 140.5, 274.65, 5.0, 92.3);
 
 INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
 VALUES (3, '2019-03-31 14:00:00', 114.6, 273.84999999999997, 7.2999999999999545, 82.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (4, '2019-03-31 15:00:00', 176.5, 274.95, 4.2000000000000455, 76.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (5, '2019-03-31 16:00:00', 140.5, 274.65, 5.0, 92.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (6, '2019-03-31 17:00:00', 114.6, 273.84999999999997, 7.2999999999999545, 82.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (7, '2019-03-31 18:00:00', 176.5, 274.95, 4.2000000000000455, 76.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (8, '2019-03-31 19:00:00', 140.5, 274.65, 5.0, 92.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (9, '2019-03-31 20:00:00', 114.6, 273.84999999999997, 7.2999999999999545, 82.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (10, '2019-03-31 21:00:00', 176.5, 274.95, 4.2000000000000455, 76.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (11, '2019-03-31 22:00:00', 140.5, 274.65, 5.0, 92.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (12, '2019-03-31 23:00:00', 114.6, 273.84999999999997, 7.2999999999999545, 82.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (13, '2019-04-01 00:00:00', 176.5, 274.95, 4.2000000000000455, 76.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (14, '2019-04-01 01:00:00', 140.5, 274.65, 5.0, 92.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (15, '2019-04-01 02:00:00', 114.6, 273.84999999999997, 7.2999999999999545, 82.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (16, '2019-04-01 03:00:00', 176.5, 274.95, 4.2000000000000455, 76.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (17, '2019-04-01 04:00:00', 140.5, 274.65, 5.0, 92.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (18, '2019-04-01 05:00:00', 114.6, 273.84999999999997, 7.2999999999999545, 82.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (19, '2019-04-01 06:00:00', 176.5, 274.95, 4.2000000000000455, 76.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (20, '2019-04-01 07:00:00', 140.5, 274.65, 5.0, 92.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (21, '2019-04-01 08:00:00', 114.6, 273.84999999999997, 7.2999999999999545, 82.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (22, '2019-04-01 09:00:00', 176.5, 274.95, 4.2000000000000455, 76.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (23, '2019-04-01 10:00:00', 140.5, 274.65, 5.0, 92.3);
+
+INSERT INTO SENSOR_DATA (id, date, sensor1, sensor2, sensor3, sensor4)
+VALUES (24, '2019-04-01 11:00:00', 114.6, 273.84999999999997, 7.2999999999999545, 82.3);
